4bd3c14a52954c2583cdff56b013c7a097e41a56
Geziel Carvalho <geziel.natal@gmail.com>
2022-07-13 17:42:35 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,12 @@
+import NotificationButton from "./components/NotificationButton";
+
 const App = () => {
   return (
+    <>
     <h1>Clean React Project</h1>
+    <NotificationButton/>
+    <NotificationButton/>
+    </>
   );
 }
 
